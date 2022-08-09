d546ffb59cc4e69be6389c1099fa75fe8412e568
thisIsJooS <thisis.joos@gmail.com>
2022-08-09 15:19:50 +0900

    jUnit 테스트를 이용해 db에 다량의 데이터 넣기

---
+++ src/test/java/com/springstudy/boot/MakeManyQuestions.java
@@ -0,0 +1,21 @@
+package com.springstudy.boot;
+
+import com.springstudy.boot.service.QuestionService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MakeManyQuestions {
+    @Autowired
+    private QuestionService questionService;
+
+    @Test
+    void testJpa() {
+        for (int i = 1; i <= 300; i++) {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content = "내용무";
+            this.questionService.create(subject, content);
+        }
+    }
+}

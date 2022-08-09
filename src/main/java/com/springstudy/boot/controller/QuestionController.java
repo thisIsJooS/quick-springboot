bf8a4b1f33c51bfea4e493f514c7ac4c583cd818
thisIsJooS <thisis.joos@gmail.com>
2022-08-09 13:58:26 +0900

    question create

--- src/main/java/com/springstudy/boot/controller/QuestionController.java
+++ src/main/java/com/springstudy/boot/controller/QuestionController.java
@@ -6,9 +6,7 @@ import com.springstudy.boot.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,5 +29,16 @@ public class QuestionController {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @GetMapping("/create")
+    public String questionCreate(){
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@RequestParam String subject, @RequestParam String content){
+        this.questionService.create(subject, content);
+        return "redirect:/question/list"; // 질문 저장 후 질문 목록으로 이동
     }
 }

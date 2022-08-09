fc4a515d73fa5ef3ad362a59f04980f5c7402ba0
thisIsJooS <thisis.joos@gmail.com>
2022-08-09 14:14:47 +0900

    validation - question form

--- src/main/java/com/springstudy/boot/controller/QuestionController.java
+++ src/main/java/com/springstudy/boot/controller/QuestionController.java
@@ -1,13 +1,16 @@
 package com.springstudy.boot.controller;
 
 import com.springstudy.boot.domain.Question;
+import com.springstudy.boot.form.QuestionForm;
 import com.springstudy.boot.repository.QuestionRepository;
 import com.springstudy.boot.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -32,13 +35,23 @@ public class QuestionController {
     }
 
     @GetMapping("/create")
-    public String questionCreate(){
+    public String questionCreate(QuestionForm questionForm){
         return "question_form";
     }
 
+//    @PostMapping("/create")
+//    public String questionCreate(@RequestParam String subject, @RequestParam String content){
+//        this.questionService.create(subject, content);
+//        return "redirect:/question/list"; // 질문 저장 후 질문 목록으로 이동
+//    }
+
     @PostMapping("/create")
-    public String questionCreate(@RequestParam String subject, @RequestParam String content){
-        this.questionService.create(subject, content);
-        return "redirect:/question/list"; // 질문 저장 후 질문 목록으로 이동
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list";
+
     }
 }

7117210ef6081ba1d74e88b33c7c6650c36cd429
thisIsJooS <thisis.joos@gmail.com>
2022-08-09 09:35:51 +0900

    redirect

---
+++ src/main/java/com/springstudy/boot/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.springstudy.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String root(){
+        return "redirect:/question/list";
+    }
+}
